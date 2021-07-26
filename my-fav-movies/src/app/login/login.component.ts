f7658b3c2e8b4f69bd0f369f0e107caf08da194f
Muthu Kumar <mk-dev@outlook.com>
2021-07-26 23:39:01 +0800

    updates

--- my-fav-movies/src/app/login/login.component.ts
+++ my-fav-movies/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -12,4 +13,9 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  login(loginForm: NgForm) {
+    console.log(loginForm.valid);
+    console.log(loginForm.value);
+  }
 }
---
+++ my-fav-movies/src/app/models/movie.ts
@@ -0,0 +1,9 @@
+export interface Movie {
+  id: number;
+  movie: string;
+  genre: string[];
+  description: string;
+  yearOfMake: string;
+  imageUrl: string;
+}
+
